0d0cb7b3cf27e84d0d5737446d26d104e5f87460
Masato Suzuki <makun.s20430@icloud.com>
2023-06-05 13:54:09 +0900

    Add vercel analytics.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import './style.css';
 import Experience from './Experience/Experience';
+import { inject } from '@vercel/analytics';
 
 window.experience = new Experience(document.getElementById('canvas'));
+
+// vercel analytics
+inject();
